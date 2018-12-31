9ae115237731331b3aa4078ce6cf4df3dda34773
Wolf-Dieter Klotz <wdklotz@gmail.com>
2018-12-31 10:24:43 +0100

    initial

---
+++ projects/production/CMakeFiles/simple_bmad_program-exe.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "Fortran"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_Fortran
+  "/Users/klotz/Desktop/BMADwdk/projects/simple_bmad_program/simple_bmad_program.f90" "/Users/klotz/Desktop/BMADwdk/projects/production/CMakeFiles/simple_bmad_program-exe.dir/simple_bmad_program/simple_bmad_program.f90.o"
+  )
+set(CMAKE_Fortran_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_Fortran_TARGET_INCLUDE_PATH
+  "/opt/local/include"
+  "/usr/X11R6/include"
+  "../../production/include"
+  "/Users/klotz/Desktop/BMAD/bmad_dist_2018_1212/include"
+  "../../production/modules"
+  "/Users/klotz/Desktop/BMAD/bmad_dist_2018_1212/production/modules"
+  "/Users/klotz/Desktop/BMAD/bmad_dist_2018_1212/production/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "/Users/klotz/Desktop/BMADwdk/projects/../production/modules")
---
+++ projects/production/CMakeFiles/simple_bmad_program-exe.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/simple_bmad_program-exe.dir/simple_bmad_program/simple_bmad_program.f90.o"
+  "/Users/klotz/Desktop/BMADwdk/production/bin/simple_bmad_program.pdb"
+  "/Users/klotz/Desktop/BMADwdk/production/bin/simple_bmad_program"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang Fortran)
+  include(CMakeFiles/simple_bmad_program-exe.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
